dc886503429879c817f60e1cb7b056509e00fe60
Cesar Santos <csar.santos18@gmail.com>
2026-04-11 21:53:35 -0500

    feat: add link certifications

--- src/components/Certifications.tsx
+++ src/components/Certifications.tsx
@@ -18,10 +18,12 @@ const certColors: Record<string, { bg: string; border: string; color: string }> 
 const certs = [
   { badge: 'Oracle', name: 'Oracle Cloud Infrastructure 2025 Certified AI Foundations Associate', issuer: 'Oracle', year: '2025', url: 'https://brm-certview.oracle.com/ords/certview/ecertificate?ssn=OC7156738&trackId=OCI25AICFA&key=ee0cd7ab8848df6051db7c56e7df0aee6885d993' },
   { badge: 'Oracle', name: 'Oracle Cloud Infrastructure 2025 Certified Foundations Associate',    issuer: 'Oracle', year: '2025', url: 'https://brm-certview.oracle.com/ords/certview/ecertificate?ssn=OC7156738&trackId=OCI25FNDCFA&key=97738738fab9690ab395e89ea942241a6cf823a6' },
-  { badge: 'EBAC',   name: 'Full Stack Python Developer',                                         issuer: 'EBAC',   year: '2024', url: null },
-  { badge: 'Docker', name: 'Docker Certified Associate',                                          issuer: 'Docker', year: '—',    url: null },
-  { badge: 'K8S',    name: 'Certified Kubernetes Administrator (CKA)',                            issuer: 'CNCF',   year: '—',    url: null },
-  { badge: 'LTips',  name: 'Pick do LinuxTips (adicione o nome)',                                 issuer: 'LinuxTips', year: '—', url: null },
+  { badge: 'EBAC',   name: 'Full Stack Python Developer',                                         issuer: 'EBAC',   year: '2024', url: 'https://lms.ebaconline.com.br/certs/validate/87722-93727-83632-40336' },
+  { badge: 'EBAC',   name: 'SQL Para Análise de Dados',                                         issuer: 'EBAC',   year: '2024', url:'https://lms.ebaconline.com.br/certs/validate/32585-93727-22726-54752'},
+  { badge: 'EBAC',   name: 'Python Para Análise de Dados',                                         issuer: 'EBAC',   year: '2024', url: 'https://lms.ebaconline.com.br/certs/validate/61168-93727-50125-25063' },
+ // { badge: 'Docker', name: 'Docker Certified Associate',                                          issuer: 'Docker', year: '—',    url: null },
+  //{ badge: 'K8S',    name: 'Certified Kubernetes Administrator (CKA)',                            issuer: 'CNCF',   year: '—',    url: null },
+  //{ badge: 'LTips',  name: 'Pick do LinuxTips (adicione o nome)',                                 issuer: 'LinuxTips', year: '—', url: null },
 ]
 
 export default function Certifications() {
